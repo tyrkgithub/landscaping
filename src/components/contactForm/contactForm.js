c2680a85195b029594098d04be1ad4c0e0dfd494
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-04-03 21:56:52 +0100

    started functions for contact form

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -5,27 +5,35 @@ import "./contactForm.css";
 //function that creates a hero element for the App
 function ContactForm() {
 
-//   const nameIn
+  const nameInput = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const message = document.querySelector("#message");
+  const success = document.querySelector("#success");
+  const errorNodes = document.querySelectorAll(".error");
 
-// function validateForm(){
+function validateForm(){
 
-// }
+console.log("yes it works durrrr")
+}
 
   return (
     <div className="contactForm">
      <div id="overlay">
-        <form className="form" onSubmit="event.preventDefault(); validateForm()">
+        <form className="form"  onSubmit={(event) => {
+       event.preventDefault();
+       validateForm();
+ }   }>
             <h1>Contact Us</h1>
 
-            <label for="name">Name:</label>
+            <label htmlFor="name">Name:</label>
             <input type="text" id="name" placeholder="Your name"/>
             <small className="error"></small>
 
-            <label for="email">Email:</label>
+            <label htmlFor="email">Email:</label>
             <input type="text" id="email" placeholder="Your email"/>
             <small className="error"></small>
 
-            <label for="message">Message:</label>
+            <label htmlFor="message">Message:</label>
             <textarea id="message" placeholder="Your message" rows="6"></textarea>
             <small className="error"></small>
 
