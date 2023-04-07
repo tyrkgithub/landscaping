de0ec470e4f4a7d5f36075f6651a63e33062a10f
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-04-07 18:24:35 +0100

    added more styling for form page

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -13,28 +13,68 @@ function ContactForm() {
 
 function validateForm(){
 
-console.log("yes it works durrrr")
+  clearMessages();
+  let errorFlag = false;
+
+if(nameInput.value.length < 1){
+  errorNodes[0].innerText = "Name cannot be blank";
+  nameInput.classList.add("error-border");
+  errorFlag = true;
+}
+
+if(!emailIsValid(email.value)){
+  errorNodes[1].innerText = "Invailid email address";
+  email.classList.add("error-border");
+    errorFlag = true;
+
+}
+
+if(message.value.length < 1){
+  errorNodes[2].innerText = "Please enter message";
+  message.classList.add("error-border");
+    errorFlag = true;
+
+}
+if(!errorFlag){
+  success.innerText = "Success!";
+}
+
+}
+
+function clearMessages(){
+  for(let i = 0; i < errorNodes.length; i++){
+    errorNodes[i].innerText = "";
+  }
+  success.innerText = "";
+  nameInput.classList.remove("error-border")
+  email.classList.remove("error-border")
+  message.classList.remove("error-border")
+}
+
+function emailIsValid(email){
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
 }
 
   return (
     <div className="contactForm">
      <div id="overlay">
-        <form className="form"  onSubmit={(event) => {
+        <form target="_blank" className="form"  onSubmit={(event) => {
        event.preventDefault();
        validateForm();
- }   }>
+ }   } action="https://formsubmit.co/tiayorke@hotmail.co.uk" method="POST">
             <h1>Contact Us</h1>
 
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" placeholder="Your name"/>
+            <input type="text" name="name" id="name" placeholder="Your name"/>
             <small className="error"></small>
 
             <label htmlFor="email">Email:</label>
-            <input type="text" id="email" placeholder="Your email"/>
+            <input type="email" name="email" id="email" placeholder="Your email"/>
             <small className="error"></small>
 
             <label htmlFor="message">Message:</label>
-            <textarea id="message" placeholder="Your message" rows="6"></textarea>
+            <textarea type="message" name="message" id="message" placeholder="Your message" rows="6"></textarea>
             <small className="error"></small>
 
             <div className="center">
