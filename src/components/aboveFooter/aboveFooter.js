3215c795e79345b1fdf969e42934219309f7dc10
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-04-11 17:56:30 +0100

    added media queries to location and get in touch

--- src/components/aboveFooter/aboveFooter.js
+++ src/components/aboveFooter/aboveFooter.js
@@ -7,9 +7,9 @@ import { Link } from "react-router-dom";
 function AboveFooter() {
   return (
     <div className="aboveFooter">
-      <h4>Call Today</h4>
+      <h4>GET IN TOUCH</h4>
       <Link className="number" to="tel:07985922312">07985922312</Link>
-      
+      <p>Call today for your FREE quote - No hassle. No obligations. No Fuss. </p>
     </div>
   );
 }
---
+++ src/components/bAndA/bAndA.js
@@ -0,0 +1,18 @@
+// importing various folders/libraries to use here
+import React from "react";
+import "./bAndA.css";
+
+
+//function that creates a hero element for the App
+function BAndA() {
+  return (
+    <div className="bAndA">
+      <h4 className="h4bAndA">Before & After</h4>
+      
+      
+    </div>
+  );
+}
+
+//exports the file to be used elsewhere
+export default BAndA;
